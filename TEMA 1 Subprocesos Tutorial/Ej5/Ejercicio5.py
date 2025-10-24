9a0757857993c53bc425b6c3b2fa5244ba777725
Alcorher <111181871+alcorher@users.noreply.github.com>
2025-10-24 12:56:05 +0200

    Add AreaRectangulo script and subprocess management for area calculation

--- TEMA 1 Subprocesos Tutorial/Ej5/Ejercicio5.py
+++ TEMA 1 Subprocesos Tutorial/Ej5/Ejercicio5.py
@@ -5,3 +5,7 @@ import time
 process = subprocess.Popen("calc.exe")
 time.sleep(5)
 process.terminate()
+while process.poll is None:
+    print("La calculadora sigue abierta.")
+   
+
---
+++ TEMA 1 Subprocesos Tutorial/Ej7/AreaRectangulo.py
@@ -0,0 +1,4 @@
+base = int(input("Introduce la base del rectángulo: "))
+altura = int(input("Introduce la altura del rectángulo: "))
+
+print (f"El área del rectangulo de base {base} y altura {altura} es {base * altura}" )
