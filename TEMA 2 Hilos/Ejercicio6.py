d95b15d6719650701c44bbe42a439c8b75d912de
Alcorher <111181871+alcorher@users.noreply.github.com>
2025-11-11 14:11:10 +0100

    added time delay in the thread loop to better visualize concurrent execution

--- TEMA 2 Hilos/Ejercicio6.py
+++ TEMA 2 Hilos/Ejercicio6.py
@@ -1,8 +1,10 @@
 import threading
+import time
 
 def mostrar_numeros(nombre, inicio, fin):
     for i in range(inicio, fin):
         print(f"{nombre}: {i}")
+        time.sleep(0.3)
 
 t1 = threading.Thread(target=mostrar_numeros, args=("Hilo 1", 1, 10))
 t2 = threading.Thread(target=mostrar_numeros, args=("Hilo 2", 20, 30))
